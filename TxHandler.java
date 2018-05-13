1f83bf12b00ee1d6adde172e2f34ded9d00f33ea
devjunhong <unique3801@gmail.com>
2018-05-13 20:51:44 +0900

    add valid conditions

--- TxHandler.java
+++ TxHandler.java
@@ -1,5 +1,8 @@
-public class TxHandler {
+import java.util.ArrayList;
+import java.security.PublicKey;
 
+public class TxHandler {
+    private UTXOPool current;
     /**
      * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
      * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
@@ -7,6 +10,7 @@ public class TxHandler {
      */
     public TxHandler(UTXOPool utxoPool) {
         // IMPLEMENT THIS
+        current = new UTXOPool(utxoPool);
     }
 
     /**
@@ -20,6 +24,9 @@ public class TxHandler {
      */
     public boolean isValidTx(Transaction tx) {
         // IMPLEMENT THIS
+        Validity validity = new Validity(tx); 
+
+        return validity.isValid();
     }
 
     /**
@@ -31,4 +38,73 @@ public class TxHandler {
         // IMPLEMENT THIS
     }
 
+    public class Validity{
+        private Transaction _tx;
+
+        public Validity(Transaction tx){
+            _tx = tx;
+        }
+
+        public boolean isValid(){
+
+        }
+
+        /**
+         * (1) all outputs claimed by {@code tx} 
+         * are in the current UTXO pool, 
+         **/
+        public boolean isInUTXOpool(){
+            ArrayList<Transaction.Output> outputs = _tx.getOutputs(); 
+            ArrayList<UTXO> utxos = current.getAllUTXO();
+
+            for(int i = 0; i < utxos.size(); i++){
+
+            }
+        }
+
+        /**
+         * (2) the signatures on each input of 
+         * {@code tx} are valid
+         **/
+        public boolean areInputsValid(){
+            ArrayList<Transaction.Input> inputs = _tx.getInputs();
+            for(int i = 0; i < inputs.size(); i++){
+                Transaction.Output output = _tx.getOutput(i);
+                Transaction.Input input = _tx.getInput(i); 
+
+                PublicKey address = output.address;
+                byte[] msg = _tx.getRawDataToSign(i);
+                byte[] sig = input.signature;
+                
+                if(!Crypto.verifySignature(address, msg, sig))
+                    return false;
+            }
+            
+            return true;
+        }
+
+        /**
+         * (3) no UTXO is claimed multiple times by {@code tx},
+         **/
+
+        /**
+         * (4) Check the all {@code tx}s output values 
+         * are non-negative
+         **/
+        public boolean isNegativeOutput(){
+            ArrayList<Transaction.Output> outputs = _tx.getOutputs(); 
+            for(int i = 0; i < outputs.size(); i++){
+                if(outputs[i].value < 0)
+                    return true;
+            }
+        }
+
+        /**
+         * (5) the sum of {@code tx}s input values is 
+         * greater than or equal to the sum of its output
+         **/
+        public boolean sumInputGTE(){
+            
+        }
+    }
 }
